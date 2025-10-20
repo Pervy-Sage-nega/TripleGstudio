c17976cac5c5a8827127b8a2fbe28ba935d4ccbb
demesis221 <rideouts199@gmail.com>
2025-10-21 03:42:26 +0800

    Add debug to build script

--- build.sh
+++ build.sh
@@ -2,7 +2,17 @@
 # exit on error
 set -o errexit
 
+echo "Current directory: $(pwd)"
+echo "Listing files:"
+ls -la
+
 pip install -r requirements.txt
+
+echo "Changing to TripleG directory"
 cd TripleG
+echo "Current directory after cd: $(pwd)"
+echo "Listing TripleG files:"
+ls -la
+
 python manage.py collectstatic --no-input
 python manage.py migrate
